45e9c960f23d199ce9ad2127aeef8bd473d7cf75
Ritik Verma <ritikverma@icloud.com>
2024-06-29 12:01:34 +0530

    TaskInput init

--- Client/src/App.jsx
+++ Client/src/App.jsx
@@ -1,10 +1,13 @@
-import { useState } from 'react'
+import TaskInput from "./components/TaskInput"
+import TaskList from "./components/TaskList"
+import "./index.css"
 
 function App() {
-
   return (
-    <div>
-      
+    <div >
+      <h1 className="text-center">To Do List</h1>
+      <TaskInput />      
+      <TaskList />
     </div>
   )
 }
--- Client/src/components/TaskInput.jsx
+++ Client/src/components/TaskInput.jsx
@@ -1,11 +1,49 @@
 import React from 'react'
+import { useState } from 'react'
+
 
 const TaskInput = () => {
-  return (
-    <div>
-      
+
+    //Using the useState hook to manage the input field, by doing this, we can see that whatever we type on the field is clearly visible to the user
+    const [enterText,setEnterText] = useState("")
+
+
+
+    //hander function to set the imput field value
+    function handleTextChange(event){
+        setEnterText(event.target.value);
+    }
+
+    //hander function for add button so that the dispatch function is activated whenever the button is clicked
+    function handleAddButton(event){
+
+    }
+
+    return (
+        //Using the Bootstrap Grid System here to contain the large input field 
+        <div className="container mt-3">
+        <div className="row justify-content-center">
+            <div className="col-md-6">
+                <div className='input-group input-group-lg'>
+                    <input
+                        className="form-control custom-box-shadow"
+                        id="inputGroup-sizing-lg"
+                        type='text'
+                        placeholder='Enter Text Here'
+                        value={enterText}
+                        onChange={handleTextChange}
+                    />
+                    <button 
+                    className='btn btn-primary btn-sm'
+                    onClick={handleAddButton}
+                    >
+                     ADD 
+                    </button>
+                </div>
+            </div>
+        </div>
     </div>
-  )
+    )
 }
 
 export default TaskInput
