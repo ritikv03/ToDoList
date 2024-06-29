ef06a8bf34c67397218f4c030a7cecdb524133d2
Ritik Verma <ritikverma@icloud.com>
2024-06-29 22:14:54 +0530

    added new modules and utilized particles.js

--- Client/src/App.jsx
+++ Client/src/App.jsx
@@ -1,3 +1,4 @@
+import ParticleBackground from "./components/ParticlesComponent"
 import TaskInput from "./components/TaskInput"
 import TaskList from "./components/TaskList"
 import "./index.css"
@@ -6,6 +7,7 @@ function App() {
   return (
     //Making header here and importing all the components
     <div >
+      <ParticleBackground/>
       <h1 className="text-center">To Do List</h1>
       <TaskInput />      
       <TaskList />
